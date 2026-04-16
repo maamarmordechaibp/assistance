26ba5693dc1ec45b377fd4ee6d5148af566ec240
maamarmordechaibp <maamarmordechaibp@gmail.com>
2026-04-16 15:05:08 -0400

    Fix open-next.config.ts with required Cloudflare overrides

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,22 @@
 const config = {
-  default: {},
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
 };
 
 export default config;
